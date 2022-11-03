c20c97ce4a7b03e7ea7aaaf5b38e6935bbe82bf5
Lucas Santos <lucasantos112291@gmail.com>
2022-11-03 20:40:30 +0000

    donde

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (pos <=this.items.length){
+      return this.items[pos];
+      } else {
+      throw new Error('OutOfBounds')
+      }
+    }
 
-  min() {}
+  max() {
+    if (this.items.length !== 0){
+      return this.items[this.items.length-1]
+      } else {
+        throw new Error('EmptySortedList')
+      }
+    }
 
-  sum() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b)
+    }
+    return 0
+  }
+
+  avg() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let total = 0;
+      for (let i in this.items) {
+        total += this.items[i];
+      }
+      let avg = total / this.items.length;
+      return avg;
+    }
+  }
 }
-
 module.exports = SortedList;
